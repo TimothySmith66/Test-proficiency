d9cda8327829e3fcc7c136289804e75d9b6d7d56
Timothy Smith <timothytsmith8@gmail.com>
2021-01-14 21:13:35 -0800

    most of manager class is done, now working on Engineer.

--- intern.js
+++
@@ -1,7 +0,0 @@
-class intern{
-    constructor(school, getSchool){
-        this.school = school;
-        this.getSchool = getSchool;
-        this.getRole = intern;
-    }
-}
--- manager.js
+++
@@ -1,6 +0,0 @@
-class manager{
-    constructor(officeNumber){
-        this.officeNumber =officeNumber;
-        this.gitRole = manager
-    } 
-}
